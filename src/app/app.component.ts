5eac395c9d90474adc91c0fba38048f527d30a88
Ryan Van Valkenburg <ryanvvb@gmail.com>
2025-02-27 15:53:00 -0700

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,27 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './components/header/header.component';
+// Angular components comprise the UI of the application
+// They are the building blocks of the application
+// Components are defined using the @Component decorator
+// The decorator provides metadata about the component
+// The metadata includes the selector, template, and styles
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent],
   template: `
-    <h1>Welcome to {{title}}!</h1>
-
-    <router-outlet />
+    <app-header />
+    <main>
+      <router-outlet></router-outlet>
+    </main>
   `,
-  styles: [],
+  styles: [`
+    main {
+      padding: 16px;
+    }
+
+    `],
 })
 export class AppComponent {
   title = 'first-ng-app';
