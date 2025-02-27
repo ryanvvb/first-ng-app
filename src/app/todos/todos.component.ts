a15a1a56aed89055ce461d02270cf662e2750a38
Ryan Van Valkenburg <ryanvvb@gmail.com>
2025-02-27 16:57:44 -0700

    Implemented search filter pipe to search for todo items containing the search string

--- src/app/todos/todos.component.ts
+++ src/app/todos/todos.component.ts
@@ -3,16 +3,19 @@ import { TodosService } from '../services/todos.service';
 import { Todo } from '../model/todo.type';
 import { catchError } from 'rxjs/operators';
 import { TodoItemComponent } from '../components/todo-item/todo-item.component';
+import { FormsModule } from '@angular/forms';
+import { FilterTodosPipe } from '../pipes/filter-todos.pipe';
 
 @Component({
   selector: 'app-todos',
-  imports: [TodoItemComponent],
+  imports: [TodoItemComponent, FormsModule, FilterTodosPipe],
   templateUrl: './todos.component.html',
   styleUrl: './todos.component.scss'
 })
 export class TodosComponent implements OnInit{
   todoService = inject(TodosService);
   todoItems = signal<Array<Todo>>([]);
+  searchTeam = signal('');
 
   ngOnInit(): void {
     this.todoService
